4653d74782d4ed151e4ba2320551d77c66ba4946
Gabrielle Warshay <gsw526@gmail.com>
2018-07-02 18:45:04 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,25 @@
 //define functions here
+function getIt(){
+  $("p").on("click"), function(){
+    window.alert("Hey!");
+  };
+}
 
+function frameIt(){
+  $("img").on("load", function(){
+    $("img").addClass("tasty");
+  });
+}
+
+function pressIt(){
+  $("input").on("keydown", function (e)
+  if(e.which === 71){
+    window.alert("G was pressed.");
+  }
+  });
+  }
+  
+  
 $(document).ready(function(){
 
 // call functions here
